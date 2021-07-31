571cc554ef8960fba0240cafaee5b2a4abbcbf06
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-31 13:08:52 +0900

    mapped several links to go from home to starred and other way around

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const LINKS = [
+  { to: "/", text: "Home" },
+  { to: "/starred", text: "Starred" },
+];
+
 const Nav = () => {
   return (
     <div>
       <ul>
-        <li>
-          <Link to="/starred">Go to starred page</Link>
-        </li>
+        {LINKS.map((link) => (
+          <li key={link.to}>
+            <Link to={link.to}>{link.text}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
