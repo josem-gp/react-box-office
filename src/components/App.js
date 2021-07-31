58bae1994dc9181e6ea6404ba964f31cd657f0a1
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-31 12:54:52 +0900

    added a default route

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ function App() {
       <Route exact={true} path="/">
         This is home page
       </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>Not found: 404 page</Route>
     </Switch>
   );
 }
