9b734483207a0bf3419242adc59cb57b8e307cba
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-31 13:04:17 +0900

    added a link to connect root to starred page

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import Nav from "./Nav";
+
 function App() {
   return (
-    <Switch>
-      <Route exact={true} path="/">
-        This is home page
-      </Route>
+    <div>
+      <Nav />
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+      <Switch>
+        <Route exact={true} path="/">
+          This is home page
+        </Route>
 
-      <Route>Not found: 404 page</Route>
-    </Switch>
+        <Route exact path="/starred">
+          This is starred
+        </Route>
+
+        <Route>Not found: 404 page</Route>
+      </Switch>
+    </div>
   );
 }
 
