e870a79410505a1d4663d50de8ae743060dd9531
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-31 14:19:37 +0900

    onClick search button implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,18 @@ const Home = () => {
     setInput(event.target.value);
   };
 
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} value={input} />
+      <button type="button" onClick={onSearch}></button>
     </MainPageLayout>
   );
 };
